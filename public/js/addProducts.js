22f31bf1ac0934d85169f424c3cdb50b9cfb1458
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-26 22:09:41 -0800

    added addProducts.js

---
+++ public/js/addProducts.js
@@ -0,0 +1,37 @@
+'use strict'; 
+
+$(document).ready(init); 
+
+var $name, $description, $price, $addedAt, $imageurl, $category, $add;
+
+function init() {
+  $name = $('#name'); 
+  $description = $('#description'); 
+  $price = $('#price'); 
+  $addedAt = $('#addedAt'); 
+  $imageurl = $('#imageurl'); 
+  $category = $('#category'); 
+  $add = $('#add'); 
+
+  $add.click(addProduct); 
+}
+
+function addProduct() {
+  var name = $name.val();
+  var description = $description.val();
+  var price = $price.val();
+  var addedAt = $addedAt.val();
+  var imageurl = $imageurl.val();
+  var category = $category.val();
+  $.post('/products', {
+    name: name, description: description, price: price,
+    addedAt: addedAt, imageurl: imageurl, category: category
+  })
+  .success(function(data) {
+    location.replace('/products');
+  })
+  .fail(function(err) {
+    alert('Error. Check console.');
+    console.error("Error:", err);
+  });
+}
