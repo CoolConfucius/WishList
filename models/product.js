5b58f4f8143f87179027eefbbff4c802982e0b46
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-26 21:24:56 -0800

    made model, worked on the routes

---
+++ models/product.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+  name: { type: String}, 
+  description: { type: String}, 
+  price: { type: Number }, 
+  addedAt: { type: Date, default: Date.now() },
+  isPurchased: { type: Boolean , default: false }
+});
+
+var Product = mongoose.model('Todo', productSchema); 
+
+module.exports = Product; 
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,50 @@
+'use strict'; 
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var Product = require('../models/product');
+
+// get multiple products
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('req.query:', req.query);
+  Product.find(req.query, function(err, products) {
+    res.status(err ? 400 : 200).send(err || products); 
+  });
+});
+
+// get one product
+router.get('/:productId', function(req, res, next) {
+  Product.findById(req.params.productId, function(err, product) {
+    res.status(err ? 400 : 200).send(err || product); 
+  });
+});
+
+// create a new product
+router.post('/', function(req, res) {
+  var product = new Product(req.body); 
+  product.save(function(err, savedProduct) {
+    res.status(err ? 400 : 200).send(err || savedProduct); 
+  });
+});
+
+// removing a product
+router.delete('/:productId', function(req, res) {
+  Product.findById(req.params.productId, function(err, product) {
+    product.remove(function(err) {
+      res.status(err ? 400 : 200).send(err || null); 
+    });
+  });
+});
+
+// toggle
+router.put('/:productId/toggle', function(req, res) {
+  Product.findById(req.params.productId, function(err, product) {   
+    product.isCompleted = !product.isCompleted; 
+    product.save(function(err, savedProduct) {
+      res.status(err ? 400 : 200).send(err || savedProduct); 
+    });
+  });
 });
 
 module.exports = router;
