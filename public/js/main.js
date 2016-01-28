fe86d5464b8a6287cc6422b0f5116a78efd8ffb1
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-28 00:30:31 -0800

    cleaned code

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,6 @@ $(document).ready(init);
 
 function init() {
   total(); 
-  console.log($(location).attr('href'), "< the location url");
-  console.log($(location).attr('href').split('/'), "< the location url");
   $('#productList').on('click', 'tr', show);
   $('#nameGo').click(nameFilter); 
   $('#categoryGo').click(categoryFilter); 
@@ -63,9 +61,8 @@ function priceFilter(){
 
 
 // Sorting
-function sort(e){
+function sort(){
   var lasturl = $(location).attr('href').split('/')[4];
-  // var num = (e.shiftKey) ? "/-1" : "/1";
   var num = (lasturl === "1") ? "/-1" : "/1";
   var str = $(this).attr('id').toLowerCase(); 
   var send = '/' + str + num; 
